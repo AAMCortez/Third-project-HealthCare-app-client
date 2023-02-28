7426dde6678416c3dbe9f950e4c15cf6c0c31981
António Cortez <aamcortez@gmail.com>
2023-02-28 17:24:03 +0000

    CSS continuing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import {
-   Button,
    Card,
    Popover,
    PopoverArrow,
@@ -9,7 +8,8 @@ import {
    PopoverHeader,
    PopoverTrigger,
    Portal,
-   SimpleGrid,
+   Stack,
+   Text,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
@@ -26,67 +26,68 @@ function Home() {
       handlePatientDetails();
    }, []);
 
-   const beds = [1, 2, 3, 4, 5, 6];
+   const beds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
    return (
-      <SimpleGrid
-         spacing={4}
-         templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+      <Stack
+         spacing={3}
+         display="flex"
+         flexDirection="row"
+         flexWrap="wrap"
+         justifyContent="space-evenly"
+         alignItems="flex-end"
+         gridTemplateRows="repeat(auto-fill, minmax(200px, 1fr))"
       >
          {" "}
-         <div>
-            {beds.map((bedNumber) => {
-               const patient = patients?.find((p) => p.bed === bedNumber); // find patient with matching bed number
-               const imgSrc = patient
-                  ? "../images/bedfull.png"
-                  : "../images/bedempty.png"; // determine image source
-               const patientName = patient ? patient.firstName : "Empty Bed";
-               const patientLastName = patient ? patient.lastName : null;
-               const patientAge = patient ? patient.age : null;
-               const patientEpisode = patient ? patient.episode : null;
+         {beds.map((bedNumber) => {
+            const patient = patients?.find((p) => p.bed === bedNumber); // find patient with matching bed number
+            const imgSrc = patient
+               ? "../images/bedfull.png"
+               : "../images/bedempty.png"; // determine image source
+            const patientName = patient ? patient.firstName : "Empty Bed";
+            const patientLastName = patient ? patient.lastName : null;
+            const patientAge = patient ? patient.age : null;
+            const patientEpisode = patient ? patient.episode : null;
 
-               return (
-                  <Popover trigger="hover">
-                     <PopoverTrigger>
-                        <Button>
-                           <Link key={bedNumber} to={`/patient/${bedNumber}`}>
-                              <Card>
-                                 <img
-                                    style={{ width: "150px", height: "100px" }}
-                                    src={imgSrc}
-                                    alt={`bed${bedNumber}`}
-                                 />
-                              </Card>
-                           </Link>
-                        </Button>
-                     </PopoverTrigger>
-                     <Portal>
-                        <PopoverContent>
-                           <PopoverArrow />
-                           <PopoverHeader fontWeight="semibold">
-                              Bed {bedNumber}
-                           </PopoverHeader>
-                           <PopoverBody>
-                              {patient ? (
-                                 <>
-                                    <div>
-                                       Patient {patientName} {patientLastName}{" "}
-                                       {patientAge} Years old
-                                    </div>
-                                    <div>Admited with {patientEpisode}</div>
-                                 </>
-                              ) : (
-                                 patientName
-                              )}
-                           </PopoverBody>
-                           <PopoverFooter></PopoverFooter>
-                        </PopoverContent>
-                     </Portal>
-                  </Popover>
-               );
-            })}
-         </div>
-      </SimpleGrid>
+            return (
+               <Popover trigger="hover">
+                  <PopoverTrigger>
+                     <Card size="md">
+                        <Link key={bedNumber} to={`/patient/${bedNumber}`}>
+                           <img
+                              style={{ width: "150px", height: "100px" }}
+                              src={imgSrc}
+                              alt={`bed${bedNumber}`}
+                           />
+                        </Link>
+                     </Card>
+                  </PopoverTrigger>
+                  <Portal>
+                     <PopoverContent>
+                        <PopoverArrow />
+                        <PopoverHeader fontWeight="semibold">
+                           Bed {bedNumber}
+                        </PopoverHeader>
+                        <PopoverBody>
+                           {patient ? (
+                              <>
+                                 <Text>
+                                    Patient {patientName} {patientLastName}{" "}
+                                    {patientAge} Years old
+                                 </Text>
+                                 <Text>Admited with {patientEpisode}</Text>
+                              </>
+                           ) : (
+                              patientName
+                           )}
+                        </PopoverBody>
+                        <PopoverFooter></PopoverFooter>
+                     </PopoverContent>
+                  </Portal>
+               </Popover>
+            );
+         })}
+      </Stack>
    );
 }
 
--- src/pages/Patient.js
+++ src/pages/Patient.js
@@ -1,6 +1,16 @@
-import { Button } from "@chakra-ui/react";
+import {
+   Button,
+   Link,
+   Box,
+   Center,
+   Heading,
+   Text,
+   Stack,
+   useColorModeValue,
+} from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { Link as ReachLink } from "react-router-dom";
 import { dischargePatient, getPatient } from "../api";
 import { UserContext } from "../context/user.context";
 
@@ -20,21 +30,25 @@ function Patient() {
 
    async function handleDischargePatient() {
       await dischargePatient(patientId);
-      
+
       navigate("/");
    }
    const type = getUserType(loggedUser); // PROTECT OTHER ROUTES
 
    return patient ? (
       <>
-         <h3>
+         {/* <h3>
             Patient {patient.firstName} {patient.lastName}
          </h3>
          <p>{patient.age} years old</p>
          <p>Medical background of {patient.personalMedicalHistory}</p>
          <p>Regular medication {patient.regularMedication}</p>
          <p>Patient admited with {patient.episode}</p>
-         {!patient.alergies ? <p>Patient has allergies to: {patient.alergiesSpecification}</p> : <p>No allergies known</p>}
+         {!patient.alergies ? (
+            <p>Patient has allergies to: {patient.alergiesSpecification}</p>
+         ) : (
+            <p>No allergies known</p>
+         )}
          {patient.imageUrl && (
             <img
                src={patient.imageUrl}
@@ -82,7 +96,14 @@ function Patient() {
                   {patient.wound.map((wound) => {
                      return (
                         <div key={wound._id}>
-                        {wound.pictureUrl && <img src={wound.pictureUrl} width="300" height="300" alt="wound" />}
+                           {wound.pictureUrl && (
+                              <img
+                                 src={wound.pictureUrl}
+                                 width="300"
+                                 height="300"
+                                 alt="wound"
+                              />
+                           )}
                            <p>{wound.description}</p>
                            <p>{wound.treatment}</p>
                         </div>
@@ -93,17 +114,130 @@ function Patient() {
                <p>Patient has skin integrity intact</p>
             )}
             {loggedUser ? (
-               <Link to={`/wound/${patientId}`}>
+               <Link as={ReachLink} to={`/wound/${patientId}`}>
                   <Button>Add a wound</Button>
                </Link>
             ) : null}
          </div>
          {loggedUser ? (
             <Button onClick={handleDischargePatient}>Discharge patient</Button>
-         ) : null}
+         ) : null} */}
+         <>
+            <Center py={100}>
+               <Box
+                  maxW={"1000px"}
+                  w={"full"}
+                  boxShadow={"2xl"}
+                  rounded={"md"}
+                  p={6}
+                  overflow={"hidden"}
+               >
+                  <Text
+                     color={"red.500"}
+                     textTransform={"uppercase"}
+                     fontWeight={800}
+                     fontSize={"md"}
+                     letterSpacing={1.1}
+                  >
+                     Bed {patient.bed}
+                  </Text>
+                  <Heading fontSize={"3xl"} fontFamily={"header"}>
+                     {patient.firstName} {patient.lastName}
+                  </Heading>
+                  <Text fontWeight={600}>{patient.age} years old</Text>
+                  <Stack direction={["column", "row"]} spacing="24px">
+                     <Stack>
+                        <Text as="cite">
+                           <Text fontSize={"xl"} fontFamily={"body"}>
+                              Admited with{" "}
+                           </Text>
+                           {patient.episode}
+                        </Text>
+
+                        <Text color={"gray.700"}>
+                           <Text fontWeight={600}>Medical background of</Text>{" "}
+                           {patient.personalMedicalHistory}
+                        </Text>
+                        <Text color={"gray.700"}>
+                           <Text fontWeight={600}>Regular medication</Text>{" "}
+                           {patient.regularMedication}
+                        </Text>
+                     </Stack>
+                     <Stack>
+                        <Text color={"gray.700"}>
+                           <Text fontWeight={600}>Medical background of</Text>{" "}
+                           {patient.personalMedicalHistory}
+                        </Text>
+                        <Text color={"gray.700"}>
+                           <Text fontWeight={600}>Regular medication</Text>{" "}
+                           {patient.regularMedication}
+                        </Text>
+                     </Stack>
+                  </Stack>
+                  <Stack mt={6} direction={"row"} spacing={4} align={"center"}>
+                     <Stack direction={"column"} spacing={0} fontSize={"sm"}>
+                        {!patient.alergies ? (
+                           <>
+                              <Text fontWeight={600}>
+                                 Patient has allergies to:
+                              </Text>{" "}
+                              <Text>{patient.alergiesSpecification}</Text>
+                           </>
+                        ) : (
+                           <Text>No allergies known</Text>
+                        )}
+                     </Stack>
+                  </Stack>
+
+                  {type.isNurse && (
+                     <Button
+                        onClick={() => navigate(`/interventions/${patientId}`)}
+                        flex={2}
+                        fontSize={"sm"}
+                        rounded={"full"}
+                        bg={"blue.400"}
+                        color={"white"}
+                        boxShadow={
+                           "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+                        }
+                        _hover={{
+                           bg: "blue.500",
+                        }}
+                        _focus={{
+                           bg: "blue.500",
+                        }}
+                     >
+                        Add interventions to the Plan
+                     </Button>
+                  )}
+                  {type.isDr && (
+                     <Button
+                        onClick={() => navigate(`/meds/${patientId}`)}
+                        flex={2}
+                        fontSize={"sm"}
+                        size={"lg"}
+                        rounded={"full"}
+                        bg={"blue.400"}
+                        color={"white"}
+                        boxShadow={
+                           "1px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+                        }
+                        _hover={{
+                           bg: "blue.500",
+                        }}
+                        _focus={{
+                           bg: "blue.500",
+                        }}
+                     >
+                        Add medication
+                     </Button>
+                  )}
+               </Box>
+            </Center>
+         </>
       </>
    ) : (
-      <Link to="../patient/admit">
+      <Link as={ReachLink} to="../patient/admit">
          <Button>Admit Patient </Button>
       </Link>
    );
